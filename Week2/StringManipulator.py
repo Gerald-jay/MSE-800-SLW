b6a30e37068adba6aa41158cb65eee11cdda9624
Gerald.Jay <gerald.jay000@gmail.com>
2025-08-10 11:15:21 +1200

    Create StringManipulator.py

---
+++ Week2/StringManipulator.py
@@ -0,0 +1,20 @@
+class StringManipulator:
+    def __init__(self, text):
+        self.text = text
+
+    def find_character(self, char):
+        return self.text.find(char)
+    
+    def print_length(self):
+        print("Length of string:", len(self.text))
+
+    def convert_to_uppercase(self):
+        print("Uppercase string:", self.text.upper())
+
+name = StringManipulator("example")
+
+result = name.find_character("x")
+print("Index of char: ", result)
+
+name.print_length()
+name.convert_to_uppercase()
