55b5e225f05eb67f9e3b7e33bfd4b23f2fdd9e34
Gerald.Jay <gerald.jay000@gmail.com>
2025-08-10 12:23:50 +1200

    Create SentenceClass.py

---
+++ Week2/SentenceClass.py
@@ -0,0 +1,21 @@
+class sentenceClass:
+    def set_sentence(self,sentence):
+        self.sentence = sentence
+
+    def count_words(self):
+        words = self.sentence
+        return len(words)
+    
+    def input_sentence(self):
+        sentence = input("Plese input your setence:")
+        if not sentence.strip():
+            print("You entered an empty sentence.")
+            self.input_sentence()
+            
+        return sentence
+
+if __name__ == "__main__":
+    analyzer = sentenceClass()
+    analyzer.set_sentence(analyzer.input_sentence())
+    print("Word count:", analyzer.count_words())
+
