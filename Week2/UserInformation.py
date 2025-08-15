84f32d8a6543d1101ab8caf796c87504137efa5f
Gerald.Jay <gerald.jay000@gmail.com>
2025-08-15 23:14:21 +1200

    Update UserInformation.py

--- Week2/UserInformation.py
+++ Week2/UserInformation.py
@@ -1,18 +1,33 @@
 class UserInformation:
-    def __init__(self, name, age, address):
-        self.name = name
-        self.age = age
-        self.address = address
 
     def input_information(self):
-        self.name = input("Please input your name:")
-        self.age = int(input("Please input your age:"))
-        self.address = input("Please input your address")
+        self.name = input("Please input your name: ")
+        age_input = input("Please input your age: ")
+        self.check_age(age_input)
+        self.address = input("Please input your address: ")
         self.personal_details()
 
+    def check_age(self, age_input):
+        while(not age_input.strip().isdigit()):
+            age_input = input("Please input your age number:")
+        self.age = int(age_input)
+
     def personal_details(self):
-        personals = [self.name, self.age, self.address]
-        return personals
+        self.personals = [self.name, self.age, self.address]
     
-    
+    def print_details(self):
+        self.personal_details()
+        print(f"The details is: name: {self.personals[0]}, age: {self.personals[1]}, address: {self.personals[2]}")
         
+    def year_add(self):
+        years = input("\nHow many years do you want to add to your age? ")
+        while(not years.strip().isdigit()):
+            years = input("Please input your age number:")
+        self.personals[1] += int(years)
+        print(f"\nAfter {years} years, {self.personals[0]} will be {self.personals[1]} years old and live at {self.personals[2]}.")
+
+if __name__ == "__main__":
+    userInfo = UserInformation()
+    userInfo.input_information()
+    userInfo.print_details()
+    userInfo.year_add()
